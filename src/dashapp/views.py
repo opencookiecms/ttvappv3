e50b3ca6b6ee5c7f148be4c9c4c0e36a5d7e4900
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2020-10-12 17:24:57 +0800

    upload

--- src/dashapp/views.py
+++ src/dashapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from vidgear.gears import CamGear
+from django.http import StreamingHttpResponse, HttpResponse, HttpResponseServerError
+from .cameraObj import cameraobject
 from .models import Ttvproject,Ttvcell,Cctvgroup,Cameraset,Cctvline,InventoryProduct,Groupcell
 
 # Create your views here.
@@ -6,6 +9,7 @@ from .models import Ttvproject,Ttvcell,Cctvgroup,Cameraset,Cctvline,InventoryPro
 def index(request):
     return render(request, 'pages/maindashboard.html')
 
+#dashbord view
 def cctv_dashboard(request):
 
     context = {
@@ -14,6 +18,7 @@ def cctv_dashboard(request):
     }
     return render (request, 'pages/cctv_dashboard.html', context)
 
+#camera group
 def cctv_cameragroup(request, group_id):
 
     context = {
@@ -23,17 +28,43 @@ def cctv_cameragroup(request, group_id):
     }
     return render (request, 'pages/discam_bygroup.html', context)
 
+
+#####################cctv processing in here ##########################################
+
+def grab_cctv(id):
+
+    camera = Cameraset.objects.get(id=id)
+    cl = camera.camera_link1
+    print("hello"+cl)
+    
+    return cl
+
+
+def frame_renderer():
+    pass
+
+
+def image_result(request, id):
+    if request.method == 'GET':
+        try:
+            return StreamingHttpResponse(frame_renderer(id), content_type='multipart/x-mixed-replace; boundary=frame')
+        except HttpResponseServerError as e:
+            print('respone got an error'+ e)
+
+
 def cctv(request, cam_id):
 
     context = {
-        'camera': Cameraset.objects.get(id=cam_id),
+        'camera': Cameraset.objects.get(id=cam_id)
     }
     return render(request, 'pages/cctv_view.html',context)
+
+#####################cctv processing in here ##########################################
+
 
 def testcctv(request, cam_id):
     
     context = {
         'camera': Cameraset.objects.get(id=cam_id),
     }
-
     return render(request, 'pages/v_test.html',context)
